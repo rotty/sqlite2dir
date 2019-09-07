6e9e7dfcb109249c35815e0f2ae7ebddce1cd1b6
Andreas Rottmann <a.rottmann@gmx.at>
2019-09-08 01:11:32 +0200

    Add Emacs settings

---
+++ .dir-locals.el
@@ -0,0 +1,15 @@
+;; This file contains recommended Emacs settings for working on this
+;; project.
+;;
+;; See the Emacs Manual node "Directory Variables" for information on
+;; how this mechanism works.
+;;
+;; Note: I'm usually not in favor of including stuff that might be
+;; specific to a developer's specific workflow in a git repository,
+;; but since this is currently a personal toy project, and I deem the
+;; settings here generally useful, I opted for including this file. If
+;; you happen to be a fellow Emacs user and take offence on the
+;; contents of this file, I'll be glad to address your concerns in the
+;; project issue you will doubtlessly open ;-).
+
+((rust-mode . ((rust-format-on-save . t))))
