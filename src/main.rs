4a419ae4e0e8df4287dc325b4adfa8b35dab04b1
Andreas Rottmann <a.rottmann@gmx.at>
2019-09-07 23:53:44 +0200

    rustfmt, don't panic on blobs

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::{
+    fmt,
     fs::{self, File},
     io::{self, Write},
     path::{Path, PathBuf},
@@ -24,11 +25,13 @@ struct Db {
 impl Db {
     fn open(path: impl AsRef<Path>) -> rusqlite::Result<Self> {
         let conn = Connection::open_with_flags(path, OpenFlags::SQLITE_OPEN_READ_ONLY)?;
-        Ok(Db {conn })
+        Ok(Db { conn })
     }
 
     fn read_schema(&self) -> rusqlite::Result<Vec<SchemaEntry>> {
-        let mut sqlite_master = self.conn.prepare("SELECT type, name, tbl_name, sql FROM sqlite_master")?;
+        let mut sqlite_master = self
+            .conn
+            .prepare("SELECT type, name, tbl_name, sql FROM sqlite_master")?;
         let schema_iter = sqlite_master.query_map(NO_PARAMS, |row| self.read_schema_entry(row))?;
         let entries = schema_iter.collect::<Result<_, _>>()?;
         Ok(entries)
@@ -37,7 +40,9 @@ impl Db {
     fn read_schema_entry(&self, row: &Row) -> rusqlite::Result<SchemaEntry> {
         let name = row.get(1)?;
         let mut tbl_info = self.conn.prepare(&format!("PRAGMA table_info({})", name))?;
-        let column_names = tbl_info.query_map(NO_PARAMS, |row| row.get(1))?.collect::<Result<_, _>>()?;
+        let column_names = tbl_info
+            .query_map(NO_PARAMS, |row| row.get(1))?
+            .collect::<Result<_, _>>()?;
         Ok(SchemaEntry {
             kind: row.get(0)?,
             name,
@@ -49,7 +54,9 @@ impl Db {
 
     fn read_table(&self, schema: &SchemaEntry) -> rusqlite::Result<TableReader> {
         Ok(TableReader {
-            stmt: self.conn.prepare(&format!("SELECT * FROM {}", schema.name))?,
+            stmt: self
+                .conn
+                .prepare(&format!("SELECT * FROM {}", schema.name))?,
         })
     }
 }
@@ -106,7 +113,9 @@ impl Sink for DirSink {
         Ok(())
     }
     fn open_table(&mut self, name: impl AsRef<str>) -> io::Result<FileTable> {
-        Ok(FileTable { file: self.open_file(format!("data/table/{}.json", name.as_ref()))? })
+        Ok(FileTable {
+            file: self.open_file(format!("data/table/{}.json", name.as_ref()))?,
+        })
     }
 }
 
@@ -114,25 +123,42 @@ struct FileTable {
     file: File,
 }
 
+#[derive(Debug)]
+enum UnsupportedInput {
+    Blob,
+}
+
+impl fmt::Display for UnsupportedInput {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            UnsupportedInput::Blob => write!(f, "blobs not yet supported"),
+        }
+    }
+}
+
+impl std::error::Error for UnsupportedInput {}
+
 impl TableSink for FileTable {
     fn write_row(&mut self, row: &Row) -> io::Result<()> {
         use rusqlite::types::ValueRef::*;
         // TODO: this could probably be made more efficient by using a
         // lower-level serialization interface.
-        let values: Vec<_> = (0..row.column_count()).map(|i| {
-            match row.get_raw(i) {
+        let values: Vec<_> = (0..row.column_count())
+            .map(|i| match row.get_raw(i) {
                 Null => Ok(json::Value::Null),
                 Integer(n) => Ok(json::Value::from(n)),
                 Real(n) => Ok(json::Value::from(n)),
                 Text(bytes) => {
-                    let text = String::from_utf8(bytes.to_vec()).map_err(|e| {
-                        io::Error::new(io::ErrorKind::InvalidInput, e)
-                    })?;
+                    let text = String::from_utf8(bytes.to_vec())
+                        .map_err(|e| io::Error::new(io::ErrorKind::InvalidInput, e))?;
                     Ok(json::Value::String(text))
-                },
-                _ => unimplemented!()
-            }
-        }).collect::<Result<_, io::Error>>()?;
+                }
+                _ => Err(io::Error::new(
+                    io::ErrorKind::InvalidInput,
+                    UnsupportedInput::Blob,
+                )),
+            })
+            .collect::<Result<_, io::Error>>()?;
         json::to_writer(&mut self.file, &values)?;
         self.file.write_all(b"\n")?;
         Ok(())
